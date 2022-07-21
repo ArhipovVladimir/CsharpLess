bbc67b3a77d5edf78cd514a7dbf1728625627862
ArhipovVS <Arhipof05@yandex.ru>
2022-07-21 22:41:42 +0700

    исправил рандои в ынес зв цикл

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -2,9 +2,10 @@
 {
     int length = collection.Length;
     int index = 0;
+    Random r = new Random();
     while (index < length)
     {
-        collection[index] = new Random().Next(1, 10);
+        collection[index] = r.Next(1, 10);
         index++;
     }
 }
